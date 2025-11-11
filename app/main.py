b656292b2d3b85470af81744ccccf71a39a26041
matt <10769515+whosmatt@users.noreply.github.com>
2025-11-12 00:32:26 +0100

    disable debug by default

--- app/main.py
+++ app/main.py
@@ -65,4 +65,4 @@ def download():
         abort(502, f"Download failed: {e}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=os.environ.get("DEBUG", False))
