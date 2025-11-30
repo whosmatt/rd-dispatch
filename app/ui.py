924f37401833bbd25657cd151da8772af7a1ed34
matt <10769515+whosmatt@users.noreply.github.com>
2025-11-30 21:51:34 +0100

    update strings

--- app/ui.py
+++ app/ui.py
@@ -33,7 +33,7 @@ def render_form(error=None):
     content = [
         H3(A("rd-dispatch", href="https://github.com/whosmatt/rd-dispatch")),
         DividerSplit(cls="h-3 mb-2"),
-        H6("Download any link ", A("supported", href="/hosts", cls=(AT.primary)), " by Real-Debrid (no Torrents ...yet)", cls=(TextPresets.muted_sm, "mb-4")),
+        H6("Download any link ", A("supported", href="/hosts", cls=(AT.primary)), " by Real-Debrid. Torrents are supported via magnet links.", cls=(TextPresets.muted_sm, "mb-4")),
         form
     ]
     container = Container(*content, cls="max-w-xl mx-auto mt-8")
